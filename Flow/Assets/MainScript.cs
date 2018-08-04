d0d21cf31b560b3bd8abb259cc877814d4ad3cb2
erich.gluck <erichgluck@gmail.com>
2018-08-04 17:55:57 +0200

    Added disco mode as a demonstration of enabling the use of a shader on a quad rendered to the screen

---
+++ Flow/Assets/MainScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+    public MeshRenderer mesh_renderer;
+    public Material disco_material;
+    RenderTexture render_texture;
+    RenderTexture disco_texture;
+    Texture2D initial_data;
+
+    int size = 128;
+
+	// Use this for initialization
+	void Start () {
+        // Set up textures for use
+        render_texture = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
+        disco_texture = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
+        mesh_renderer.material.SetTexture("_MainTex", render_texture);
+
+        // Initialize color to red
+        initial_data = new Texture2D(size, size);
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                initial_data.SetPixel(x, y, Color.red);
+            }
+        }
+        initial_data.Apply();
+        Graphics.Blit(initial_data, render_texture);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Graphics.Blit(render_texture, disco_texture, disco_material);
+        Graphics.Blit(disco_texture, render_texture);
+    }
+}
