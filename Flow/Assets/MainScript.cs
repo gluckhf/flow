f5abc9078f3f445048b718c1da6d383e8560efce
erich.gluck <erichgluck@gmail.com>
2018-08-04 18:34:06 +0200

    Removed disco in favor of water flow simulation that (currently) averages the R (amount) value of "water" in each pixel with the surrounding pixels

--- Flow/Assets/MainScript.cs
+++ Flow/Assets/MainScript.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class MainScript : MonoBehaviour {
 
     public MeshRenderer mesh_renderer;
-    public Material disco_material;
     RenderTexture render_texture;
-    RenderTexture disco_texture;
     Texture2D initial_data;
 
-    int size = 128;
+    // Required for using the water shader
+    public Material water_material;
+    RenderTexture water_texture;
+    
+
+    int width = 128;
+    int height = 64;
 
 	// Use this for initialization
 	void Start () {
+
+        transform.localScale = new Vector3(1f, (float)height / (float)width, 1f);
+
         // Set up textures for use
-        render_texture = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
-        disco_texture = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
+        render_texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
+        water_texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
         mesh_renderer.material.SetTexture("_MainTex", render_texture);
 
+        // Textures must be set to point filter
+        render_texture.filterMode = FilterMode.Point;
+        water_texture.filterMode = FilterMode.Point;
+
+        // Set the sizes of the material
+        water_material.SetFloat("_TexelWidth", 1.0f / width);
+        water_material.SetFloat("_TexelHeight", 1.0f / height);
+
+        render_texture.wrapMode = TextureWrapMode.Repeat;
+
         // Initialize color to red
-        initial_data = new Texture2D(size, size);
-        for (int x = 0; x < size; x++)
+        initial_data = new Texture2D(width, height);
+        for (int y = 0; y < height; y++)
         {
-            for (int y = 0; y < size; y++)
+            for (int x = 0; x < width; x++)
             {
-                initial_data.SetPixel(x, y, Color.red);
+                initial_data.SetPixel(x, y, new Color(Random.Range(0f,1f),0,0));
             }
         }
         initial_data.Apply();
@@ -35,7 +52,7 @@ public class MainScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Graphics.Blit(render_texture, disco_texture, disco_material);
-        Graphics.Blit(disco_texture, render_texture);
+        Graphics.Blit(render_texture, water_texture, water_material);
+        Graphics.Blit(water_texture, render_texture);
     }
 }
--- Flow/Assets/WaterShader.shader
+++ Flow/Assets/WaterShader.shader
@@ -1,8 +1,10 @@
-﻿Shader "Unlit/Disco"
+﻿Shader "Flow/Water"
 {
 	Properties
 	{
 		_MainTex ("Texture", 2D) = "white" {}
+		_TexelWidth ("TexelWidth", float) = 0
+		_TexelHeight ("TexelHeight", float) = 0
 	}
 	SubShader
 	{
@@ -29,8 +31,11 @@
 				float4 vertex : SV_POSITION;
 			};
 
+			// Must be redeclared from Properties to be able to be used
 			sampler2D _MainTex;
 			float4 _MainTex_ST;
+			float _TexelWidth;
+			float _TexelHeight;
 			
 			v2f vert (appdata v)
 			{
@@ -43,14 +48,16 @@
 			fixed4 frag (v2f i) : SV_Target
 			{
 				// sample the texture
-				fixed4 col = tex2D(_MainTex, i.uv);
-				col.r += 0.1;
-				if(col.r > 0.5)
-				{
-					col.r = 0;
-				}
+				fixed4 pixel = tex2D(_MainTex, i.uv);
 
-				return col;
+				fixed4 pixel_n = tex2D(_MainTex, i.uv + fixed2(0,_TexelHeight));
+				fixed4 pixel_e = tex2D(_MainTex, i.uv + fixed2(_TexelWidth,0));
+				fixed4 pixel_s = tex2D(_MainTex, i.uv - fixed2(0,_TexelHeight));
+				fixed4 pixel_w = tex2D(_MainTex, i.uv - fixed2(_TexelWidth,0));
+
+				pixel.r = (pixel_n.r + pixel_e.r +  pixel_s.r + pixel_w.r) / 4.0;
+
+				return pixel;
 			}
 			ENDCG
 		}
