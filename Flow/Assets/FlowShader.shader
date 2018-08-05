c3efc9998bda16c48706b784e142823908106e3f
erich.gluck <erichgluck@gmail.com>
2018-08-05 16:56:24 +0200

    Removed individual shaders in favor of one generic shader with run-time initialization of parameters. Changed list of materials and textures to arrays in MainScript

---
+++ Flow/Assets/FlowShader.shader
@@ -0,0 +1,95 @@
+﻿Shader "Flow/Flow"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_HeightTex ("HeightTex", 2D) = "white" {}
+		_TexelWidth ("TexelWidth", float) = 0
+		_TexelHeight ("TexelHeight", float) = 0
+		_NumElements ("NumElements", float) = 1
+		_FlowDivisor ("FlowDivisor", float) = 1
+		_FlowGradient ("FlowGradient", float) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			// Must be redeclared from Properties to be able to be used
+			sampler2D _MainTex;
+			sampler2D _HeightTex;
+			float4 _MainTex_ST;
+			float _TexelWidth;
+			float _TexelHeight;
+			float _NumElements;
+			float _FlowDivisor;
+			float _FlowGradient;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			float4 frag (v2f i) : SV_Target
+			{
+				// sample this texture pixels
+				float4 this_pixel = tex2D(_MainTex, i.uv);
+				float4 this_pixel_n = tex2D(_MainTex, i.uv + fixed2(0,_TexelHeight));
+				float4 this_pixel_e = tex2D(_MainTex, i.uv + fixed2(_TexelWidth,0));
+				float4 this_pixel_s = tex2D(_MainTex, i.uv - fixed2(0,_TexelHeight));
+				float4 this_pixel_w = tex2D(_MainTex, i.uv - fixed2(_TexelWidth,0));
+
+				// sample the height pixels
+				float4 height_pixel = tex2D(_HeightTex, i.uv) * _NumElements;
+				float4 height_pixel_n = tex2D(_HeightTex, i.uv + fixed2(0,_TexelHeight)) * _NumElements;
+				float4 height_pixel_e = tex2D(_HeightTex, i.uv + fixed2(_TexelWidth,0)) * _NumElements;
+				float4 height_pixel_s = tex2D(_HeightTex, i.uv - fixed2(0,_TexelHeight)) * _NumElements;
+				float4 height_pixel_w = tex2D(_HeightTex, i.uv - fixed2(_TexelWidth,0)) * _NumElements;
+				
+				this_pixel.r = this_pixel.r
+				
+				// North
+				- max(min((height_pixel.r + _FlowGradient) - height_pixel_n.r, this_pixel.r) / _FlowDivisor, 0) // assume that this cell has more "height"
+				+ max(min(height_pixel_n.r - (height_pixel.r + _FlowGradient), this_pixel_n.r) / _FlowDivisor, 0) // assume that the other cell has more "height"
+				
+				// East
+				- max(min(height_pixel.r - height_pixel_e.r, this_pixel.r) / _FlowDivisor, 0) // assume that this cell has more "height"
+				+ max(min(height_pixel_e.r - height_pixel.r, this_pixel_e.r) / _FlowDivisor, 0) // assume that the other cell has more "height"
+				
+				// South
+				- max(min(height_pixel.r - (height_pixel_s.r + _FlowGradient), this_pixel.r) / _FlowDivisor, 0) // assume that this cell has more "height"
+				+ max(min((height_pixel_s.r + _FlowGradient) - height_pixel.r, this_pixel_s.r) / _FlowDivisor, 0) // assume that the other cell has more "height"
+				
+				// West
+				- max(min(height_pixel.r - height_pixel_w.r, this_pixel.r) / _FlowDivisor, 0) // assume that this cell has more "height"
+				+ max(min(height_pixel_w.r - height_pixel.r, this_pixel_w.r) / _FlowDivisor, 0); // assume that the other cell has more "height"
+				
+				return this_pixel;
+			}
+			ENDCG
+		}
+	}
+}
--- Flow/Assets/MainScript.cs
+++ Flow/Assets/MainScript.cs
@@ -15,39 +15,47 @@ public class MainScript : MonoBehaviour {
     // World compositing
     public Material world_material;
     private Material[] world_material_flip_flops = new Material[2];
-    RenderTexture render_texture;
+    RenderTexture world_texture;
 
-    // Water components
-    public Material water_material;
-    RenderTexture[] water_texture = new RenderTexture[2];
+    // Flow components
+    private enum flows
+    {
+        water = 0,
+        mud,
+        size
+    }
+    
+    public Material flow_material;
+    private Material[] flow_materials = new Material[(int)flows.size];
+    RenderTexture[,] flow_textures = new RenderTexture[2, (int)flows.size];
 
-    // Mud components
-    public Material mud_material;
-    RenderTexture[] mud_texture = new RenderTexture[2];
-
-    // Dirt components
-    public Material dirt_material;
-    RenderTexture[] dirt_texture = new RenderTexture[2];
-
+    // Solid components
+    private enum solids
+    {
+        dirt = 0,
+        size
+    }
+    
+    RenderTexture[,] solid_textures = new RenderTexture[2, (int)solids.size];
+    
     // Provides a link to the debug text
     public Text DebugText;
-    
-    int width = 128;
-    int height = 64;
 
+    // World size - powers of 2 for optimal efficiency
+    public int width = 128;
+    public int height = 64;
+
+    // A-B rendering buffer selection
     int flip_flop = 0;
+
+    // Update rate (per second) - independent of framerate
+    [Range(500f, 2000f)]
+    public float update_rate = 1000f;
 
     // Use this for initialization
     void Start() {
-
+        // Match the local scale to the scale set up in the input parameters
         transform.localScale = new Vector3(1f, (float)height / (float)width, 1f);
-        
-        // Set up the main render texture
-        render_texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-        render_texture.useMipMap = false;
-        render_texture.autoGenerateMips = false;
-        render_texture.wrapMode = TextureWrapMode.Repeat;
-        render_texture.filterMode = FilterMode.Point;
 
         height_texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
         height_texture.useMipMap = false;
@@ -55,133 +63,138 @@ public class MainScript : MonoBehaviour {
         height_texture.wrapMode = TextureWrapMode.Repeat;
         height_texture.filterMode = FilterMode.Point;
         
-        // Set up the element textures
-        // TODO: Element bank with double indexing
-        int num_elements = 0;
-        for (int i = 0; i < 2; i++)
+        // Flowables
         {
-            water_texture[i] = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
-            water_texture[i].useMipMap = false;
-            water_texture[i].autoGenerateMips = false;
-            water_texture[i].filterMode = FilterMode.Point;
-            num_elements++;
-
-            mud_texture[i] = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
-            mud_texture[i].useMipMap = false;
-            mud_texture[i].autoGenerateMips = false;
-            mud_texture[i].filterMode = FilterMode.Point;
-            num_elements++;
-
-            dirt_texture[i] = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
-            dirt_texture[i].useMipMap = false;
-            dirt_texture[i].autoGenerateMips = false;
-            dirt_texture[i].filterMode = FilterMode.Point;
-            num_elements++;
-        }
-
-        // Set up the mesh renderer
-        mesh_renderer.material.SetTexture("_MainTex", render_texture);
-        
-        // Set the sizes of the element materials
-        water_material.SetFloat("_TexelWidth", 1.0f / width);
-        water_material.SetFloat("_TexelHeight", 1.0f / height);
-        water_material.SetFloat("_NumElements", num_elements);
-
-        mud_material.SetFloat("_TexelWidth", 1.0f / width);
-        mud_material.SetFloat("_TexelHeight", 1.0f / height);
-        mud_material.SetFloat("_NumElements", num_elements);
-
-        dirt_material.SetFloat("_TexelWidth", 1.0f / width);
-        dirt_material.SetFloat("_TexelHeight", 1.0f / height);
-        dirt_material.SetFloat("_NumElements", num_elements);
-
-        // Initialize water
-        {
-            Texture2D initial_data = new Texture2D(width, height);
-            for (int y = 0; y < height; y++)
+            // Set up flowable common attributes with default "hovering" gradient
+            for (int mat = 0; mat < (int)flows.size; mat++)
             {
-                for (int x = 0; x < width; x++)
+                for (int i = 0; i < 2; i++)
                 {
-                    //initial_data.SetPixel(x, y, new Color(Random.Range(0f,1f),0,0));
-                    initial_data.SetPixel(x, y, new Color(0, 0, 0));
+                    flow_textures[i, mat] = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
+                    flow_textures[i, mat].useMipMap = false;
+                    flow_textures[i, mat].autoGenerateMips = false;
+                    flow_textures[i, mat].filterMode = FilterMode.Point;
                 }
-            }
-            initial_data.Apply();
-            Graphics.Blit(initial_data, water_texture[0]);
-            Graphics.Blit(initial_data, water_texture[1]);
-        }
 
-        // Initialize mud
-        {
-            Texture2D initial_data = new Texture2D(width, height);
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    //initial_data.SetPixel(x, y, new Color(Random.Range(0f, 1f), 0, 0));
-                    initial_data.SetPixel(x, y, new Color(0, 0, 0));
-                }
+                flow_materials[mat] = new Material(flow_material);
+                flow_materials[mat].SetTexture("_HeightTex", height_texture);
+                flow_materials[mat].SetFloat("_TexelWidth", 1.0f / width);
+                flow_materials[mat].SetFloat("_TexelHeight", 1.0f / height);
+                flow_materials[mat].SetFloat("_NumElements", (float)flows.size + (float)solids.size);
+                flow_materials[mat].SetFloat("_FlowDivisor", 5.0f + (float)flows.size);
+                flow_materials[mat].SetFloat("_FlowGradient", 0);
             }
-            initial_data.Apply();
-            Graphics.Blit(initial_data, mud_texture[0]);
-            Graphics.Blit(initial_data, mud_texture[1]);
-        }
 
-        // Initialize dirt to boxes
-        {
-            Texture2D initial_data = new Texture2D(width, height);
-            for (int y = 0; y < height; y++)
+            // Water
             {
-                for (int x = 0; x < width; x++)
+                flow_materials[(int)flows.water].SetFloat("_FlowGradient", -1.0f / height);
+
+                Texture2D initial_data = new Texture2D(width, height);
+                for (int y = 0; y < height; y++)
                 {
-                    if (y == 0 || y == 16 || y == height-1 || x == 0 || x == 16 || x == width-1)
+                    for (int x = 0; x < width; x++)
                     {
-                        initial_data.SetPixel(x, y, new Color(1, 0, 0));
-                    }
-                    else
-                    {
+                        //initial_data.SetPixel(x, y, new Color(Random.Range(0f,1f),0,0));
                         initial_data.SetPixel(x, y, new Color(0, 0, 0));
                     }
                 }
+                initial_data.Apply();
+                Graphics.Blit(initial_data, flow_textures[0, (int)flows.water]);
+                Graphics.Blit(initial_data, flow_textures[1, (int)flows.water]);
             }
-            initial_data.Apply();
-            Graphics.Blit(initial_data, dirt_texture[0]);
-            Graphics.Blit(initial_data, dirt_texture[1]);
+
+            // Mud
+            {
+                flow_materials[(int)flows.mud].SetFloat("_FlowGradient", 1.0f / height);
+
+                Texture2D initial_data = new Texture2D(width, height);
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        //initial_data.SetPixel(x, y, new Color(Random.Range(0f, 1f), 0, 0));
+                        initial_data.SetPixel(x, y, new Color(0, 0, 0));
+                    }
+                }
+                initial_data.Apply();
+                Graphics.Blit(initial_data, flow_textures[0, (int)flows.mud]);
+                Graphics.Blit(initial_data, flow_textures[1, (int)flows.mud]);
+            }
+        }
+
+        // Solids
+        {
+            // Set up solid common attributes
+            for (int mat = 0; mat < (int)solids.size; mat++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    solid_textures[i, mat] = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
+                    solid_textures[i, mat].useMipMap = false;
+                    solid_textures[i, mat].autoGenerateMips = false;
+                    solid_textures[i, mat].filterMode = FilterMode.Point;
+                }
+            }
+
+            // Dirt
+            {
+                Texture2D initial_data = new Texture2D(width, height);
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (y == 0 || y == 16 || y == height - 1 || x == 0 || x == 16 || x == width - 1)
+                        {
+                            initial_data.SetPixel(x, y, new Color(1, 0, 0));
+                        }
+                        else
+                        {
+                            initial_data.SetPixel(x, y, new Color(0, 0, 0));
+                        }
+                    }
+                }
+                initial_data.Apply();
+                Graphics.Blit(initial_data, solid_textures[0, (int)solids.dirt]);
+                Graphics.Blit(initial_data, solid_textures[1, (int)solids.dirt]);
+            }
         }
         
-        // Set up the world components which point to the now-initialized textures
-        for (int i = 0; i < 2; i++)
+        // Set up height map
         {
-            world_material_flip_flops[i] = new Material(world_material);
-            world_material_flip_flops[i].SetFloat("_TexelWidth", 1.0f / width);
-            world_material_flip_flops[i].SetFloat("_TexelHeight", 1.0f / height);
-            world_material_flip_flops[i].SetFloat("_NumElements", num_elements);
-            world_material_flip_flops[i].SetTexture("_WaterTex", water_texture[i]);
-            world_material_flip_flops[i].SetTexture("_MudTex", mud_texture[i]);
-            world_material_flip_flops[i].SetTexture("_DirtTex", dirt_texture[i]);
+            for (int i = 0; i < 2; i++)
+            {
+                height_material_flip_flops[i] = new Material(height_material);
+                height_material_flip_flops[i].SetFloat("_TexelWidth", 1.0f / width);
+                height_material_flip_flops[i].SetFloat("_TexelHeight", 1.0f / height);
+                height_material_flip_flops[i].SetFloat("_NumElements", (float)flows.size + (float)solids.size);
+                height_material_flip_flops[i].SetTexture("_WaterTex", flow_textures[i, (int)flows.water]);
+                height_material_flip_flops[i].SetTexture("_MudTex", flow_textures[i, (int)flows.mud]);
+                height_material_flip_flops[i].SetTexture("_DirtTex", solid_textures[i, (int)solids.dirt]);
+
+                Graphics.Blit(null, height_texture, height_material_flip_flops[i]);
+            }
         }
 
-        // Set up the height map components which point to the now-initialized textures
-        for (int i = 0; i < 2; i++)
+        // Set up the world renderer
         {
-            height_material_flip_flops[i] = new Material(height_material);
-            height_material_flip_flops[i].SetFloat("_TexelWidth", 1.0f / width);
-            height_material_flip_flops[i].SetFloat("_TexelHeight", 1.0f / height);
-            height_material_flip_flops[i].SetFloat("_NumElements", num_elements);
-            height_material_flip_flops[i].SetTexture("_WaterTex", water_texture[i]);
-            height_material_flip_flops[i].SetTexture("_MudTex", mud_texture[i]);
-            height_material_flip_flops[i].SetTexture("_DirtTex", dirt_texture[i]);
-        }
+            world_texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            world_texture.useMipMap = false;
+            world_texture.autoGenerateMips = false;
+            world_texture.wrapMode = TextureWrapMode.Repeat;
+            world_texture.filterMode = FilterMode.Point;
 
-        // Set up the elements to reference the height maps
-        dirt_material.SetTexture("_HeightTex", height_texture);
-        mud_material.SetTexture("_HeightTex", height_texture);
-        water_material.SetTexture("_HeightTex", height_texture);
+            mesh_renderer.material.SetTexture("_MainTex", world_texture);
 
-        // Write initial height maps
-        for (int i = 0; i < 2; i++)
-        {
-            Graphics.Blit(null, height_texture, height_material_flip_flops[i]);
+            for (int i = 0; i < 2; i++)
+            {
+                world_material_flip_flops[i] = new Material(world_material);
+                world_material_flip_flops[i].SetFloat("_TexelWidth", 1.0f / width);
+                world_material_flip_flops[i].SetFloat("_TexelHeight", 1.0f / height);
+                world_material_flip_flops[i].SetFloat("_NumElements", (float)flows.size);
+                world_material_flip_flops[i].SetTexture("_WaterTex", flow_textures[i, (int)flows.water]);
+                world_material_flip_flops[i].SetTexture("_MudTex", flow_textures[i, (int)flows.mud]);
+                world_material_flip_flops[i].SetTexture("_DirtTex", solid_textures[i, (int)solids.dirt]);
+            }
         }
     }
 	
@@ -206,10 +219,10 @@ public class MainScript : MonoBehaviour {
             RenderTexture currentActiveRT = RenderTexture.active;
 
             // Set the supplied RenderTexture as the active one
-            RenderTexture.active = water_texture[flip_flop];
+            RenderTexture.active = flow_textures[flip_flop, (int)flows.water];
 
             // Create a new Texture2D and read the RenderTexture image into it
-            Texture2D tex = new Texture2D(water_texture[flip_flop].width, water_texture[flip_flop].height);
+            Texture2D tex = new Texture2D(width, height);
             tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
             
             for (int i = -1; i < 1; i++)
@@ -224,7 +237,7 @@ public class MainScript : MonoBehaviour {
 
             // TODO: Hack? This line shouldn't be required but somehow needs to be here.
             // Also the "proper" way to do this would be to write a shader to modify the texture then run the shader
-            Graphics.Blit(tex, water_texture[flip_flop]);
+            Graphics.Blit(tex, flow_textures[flip_flop, (int)flows.water]);
 
             // Restore previously active render texture
             RenderTexture.active = currentActiveRT;
@@ -237,36 +250,44 @@ public class MainScript : MonoBehaviour {
             RenderTexture currentActiveRT = RenderTexture.active;
 
             // Set the supplied RenderTexture as the active one
-            RenderTexture.active = mud_texture[flip_flop];
+            RenderTexture.active = solid_textures[flip_flop, (int)solids.dirt];
 
             // Create a new Texture2D and read the RenderTexture image into it
-            Texture2D tex = new Texture2D(mud_texture[flip_flop].width, mud_texture[flip_flop].height);
+            Texture2D tex = new Texture2D(width, height);
             tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
             tex.SetPixel(pos_grid.x, pos_grid.y, Color.red);
             tex.Apply();
 
             // TODO: Hack? This line shouldn't be required but somehow needs to be here.
             // Also the "proper" way to do this would be to write a shader to modify the texture then run the shader
-            Graphics.Blit(tex, mud_texture[flip_flop]);
+            Graphics.Blit(tex, solid_textures[flip_flop, (int)solids.dirt]);
 
             // Restore previously active render texture
             RenderTexture.active = currentActiveRT;
         }
-
-        // Flip the buffer
-        flip_flop = 1 - flip_flop;
         
-        // Run the shaders for flowable elements
-        Graphics.Blit(water_texture[1 - flip_flop], water_texture[flip_flop], water_material);
-        Graphics.Blit(mud_texture[1 - flip_flop], mud_texture[flip_flop], mud_material);
+        // Make the simulation run a lot faster than the framerate
+        float time_step = 1f / update_rate;
+        for (float i = 0; i < Time.deltaTime; i += time_step)
+        {
+            // Flip the buffer
+            flip_flop = 1 - flip_flop;
 
-        // Dirt doesn't flow so it doesn't need a shader, but it's here for completeness and maybe use later
-        Graphics.Blit(dirt_texture[1 - flip_flop], dirt_texture[flip_flop], dirt_material);
+            // Run the shaders for flowable elements
+            Graphics.Blit(flow_textures[1 - flip_flop, (int)flows.water], flow_textures[flip_flop, (int)flows.water], flow_materials[(int)flows.water]);
+            Graphics.Blit(flow_textures[1 - flip_flop, (int)flows.mud], flow_textures[flip_flop, (int)flows.mud], flow_materials[(int)flows.mud]);
+
+            // Update the solid elements
+            for (int j = 0; j < (int)solids.size; j++)
+            {
+                Graphics.Blit(solid_textures[1 - flip_flop, j], solid_textures[flip_flop, j]); 
+            }
+
+            // Write the new height maps for use next update
+            Graphics.Blit(null, height_texture, height_material_flip_flops[flip_flop]);
+        }
 
         // Use Blit to run the world shader (which references the water and dirt textures) and save the result to render_texture
-        Graphics.Blit(null, render_texture, world_material_flip_flops[flip_flop]);
-
-        // Write the new height maps for use next frame
-        Graphics.Blit(null, height_texture, height_material_flip_flops[flip_flop]);
+        Graphics.Blit(null, world_texture, world_material_flip_flops[flip_flop]);
     }
 }
